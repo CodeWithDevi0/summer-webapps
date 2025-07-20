c1b2c0d2c4ec377ea07de36fce323b244f647a46
DeviCode <jpaquinol44@gmail.com>
2025-07-20 14:50:36 +0800

    Updated

---
+++ Idle-venture/includes/hero-section.php
@@ -0,0 +1,7 @@
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="header-title">Idle Venture</h1>
+        </div>
+    </div>
+</div>
